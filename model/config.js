67160e8ebba56d8edb15ec115319ac7f09af8001
Unknown <wlstjs4409@naver.com>
2018-06-17 11:35:33 +0900

    a

--- model/config.js
+++ model/config.js
@@ -1,5 +1,5 @@
 var dbConnectionInfo = {
-    connection: 'mysql://istream.ca6pgnlxltlv.ap-northeast-2.rds.amazonaws.com:3306/istream'
+    connection: 'mysql://istream:istreamistream@istream.ca6pgnlxltlv.ap-northeast-2.rds.amazonaws.com:3306/istream'
 };
 
 module.exports = dbConnectionInfo;
